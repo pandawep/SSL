0a0d3e763bcb795e5bbda502cac5d665ab3d34ec
pandawep <154017398+pandawep@users.noreply.github.com>
2024-02-22 15:57:14 +0530

    Create ring.js

---
+++ js/ring.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // JavaScript code to simulate a click event on the button in a loop
+    var button = document.getElementById('ring-page-play-sound');
+
+    // Function to simulate a click event on the button
+    function clickButton() {
+        if (button) {
+            button.click();
+            console.log("Button clicked!");
+        } else {
+            console.log("Button not found!");
+        }
+    }
+
+    // Set interval to continuously click the button every 3 seconds (3000 milliseconds)
+    setInterval(clickButton, 3000);
+});
